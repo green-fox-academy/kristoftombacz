c1806aed05567563ba445cd461b221d5b93d6681
Kristóf Tombácz <tombacz.kristof@gmail.com>
2016-01-13 06:56:50 +0100

    refactored the code

--- week-8/candy/script.js
+++ week-8/candy/script.js
@@ -13,80 +13,70 @@ function Candy() {
   this.creatLollipop100 = document.querySelector('.creatLollipop100');
 
   this.addCandy = addCandy;
+  this.getVelocity = getVelocity;
+  this.refresh = refresh;
+  this.ifLollipop10 = ifLollipop10;
   this.gameWon = gameWon;
   this.disableButton = disableButton;
-  this.ifLollipop10 = ifLollipop10;
-  this.refresh = refresh;
-  this.getVelocity = getVelocity;
+  this.buyLollipopFor = buyLollipopFor;
 
   var _this = this;
+  this.refresh();
 
   function addCandy(number){
     _this.candycount += number;
     _this.refresh();
   }
-
   function getVelocity(){
     return Math.floor(_this.lollipopcount / 10);
   }
-
   function refresh(){
     _this.candies.innerText = _this.candycount;
     _this.lollipops.innerText = _this.lollipopcount;
     _this.velocity.innerText = _this.velocitycount;
   }
-
   function ifLollipop10(){
     if (_this.lollipopcount >= 10){
       _this.addCandy(_this.getVelocity());
     }
   }
-
   function gameWon(number) {
     if (_this.candycount > number-1){
       alert('You WON!');
       document.location.reload();
     }
   }
-
   function disableButton(number, button){
     if (_this.candycount >= number-1){
-      button.removeAttribute('disabled');
+      button.disabled = false;
     } else {
       button.disabled = true;
+    }
+  }
+  function buyLollipopFor(number){
+    if (_this.candycount >= number){
+      _this.candycount -= number;
+      _this.lollipopcount += number / 10;
+      _this.velocitycount = _this.getVelocity();
+      _this.refresh();
     }
   }
 
   this.buyCandy.addEventListener('click', function() {
     _this.addCandy(1);
   })
-
   this.creatLollipop.addEventListener('click', function() {
-    if (_this.candycount >= 10) {
-      _this.candycount -= 10;
-      _this.lollipopcount++;
-      _this.velocitycount = _this.getVelocity();
-      _this.refresh();
-    }
+    _this.buyLollipopFor(10);
   })
-
   this.creatLollipop100.addEventListener('click', function() {
-    if (_this.candycount >= 100) {
-      _this.candycount -= 100;
-      _this.lollipopcount += 10;
-      _this.velocitycount++;
-      _this.refresh();
-    }
+    _this.buyLollipopFor(100);
   })
-
-  this.refresh();
 
   setInterval (function() {
     _this.disableButton(10, _this.creatLollipop);
     _this.disableButton(100, _this.creatLollipop100);
     _this.ifLollipop10();
     _this.gameWon(1500);
-
   }, 1000);
 }
 
